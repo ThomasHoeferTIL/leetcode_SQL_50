d1fa74a46b06137bdb39b7a3135ee8764103fb61
ThomasHoeferTIL <thomas.hoefer@theinformationlab.de>
2025-01-08 11:51:25 +0100

    Time: 384 ms (34.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+select 
+query_name
+,round(sum(rating/position::numeric)/count(distinct result),2) as quality
+,round((sum(case when rating<3 then 1 else 0 end)::numeric/count(distinct result))*100,2) as poor_query_percentage
+from queries
+group by query_name
+having count(query_name) > 0;
